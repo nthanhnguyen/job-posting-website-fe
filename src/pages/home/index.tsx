81f1f5b7a0fe7d02c91222f30f6217aacca20194
DucAnh <ducanhnv312@gmail.com>
2024-09-13 17:51:12 +0700

    JPWA-4

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,19 +3,31 @@ import styles from 'styles/client.module.scss';
 import SearchClient from '@/components/client/search.client';
 import JobCard from '@/components/client/card/job.card';
 import CompanyCard from '@/components/client/card/company.card';
+import Blog from '../blog';
 
 const HomePage = () => {
     return (
-        <div className={`${styles["container"]} ${styles["home-section"]}`}>
-            <div className="search-content" style={{ marginTop: 20 }}>
+        <div>
+
+            {/* <div className="search-content" style={{ width: '100%' }} >
                 <SearchClient />
+            </div> */}
+            <div className={`${styles["container"]} ${styles["home-section"]}`}>
+
+                <div className="search-content" style={{ marginTop: 20 }}>
+                    <SearchClient />
+                </div>
+                <Divider />
+                <CompanyCard />
+                <div style={{ margin: 50 }}></div>
+                <Divider />
+                <div>
+                    <JobCard />
+                </div>
+
+                <Blog />
             </div>
-            <Divider />
-            <CompanyCard />
-            <div style={{ margin: 50 }}></div>
-            <Divider />
-            <JobCard />
-        </div>
+        </div >
     )
 }
 
